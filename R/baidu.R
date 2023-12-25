21dc2856fafa45c6b169c8601d9871f0b2be2a2e
Biqing Li <62424380+libiqing77@users.noreply.github.com>
2023-12-25 14:28:15 +0800

    Add files via upload

--- R/baidu.R
+++ R/baidu.R
@@ -1,28 +1,23 @@
 ##' @rdname translate
 ##' @export
-baidu_translate <- function(x, from = 'en', to = 'zh') {
-    vectorize_translator(x, .baidu_translate, from = from, to = to)
-}
-
 
 ##' @importFrom jsonlite fromJSON
 ##' @importFrom httr modify_url
-.baidu_translate <- function(x, from = 'en', to = 'zh') {
+baidu_translate <- function(x, from = 'en', to = 'zh') {
+    query=paste(x,collapse = "\n")
     url <- httr::modify_url("http://api.fanyi.baidu.com/api/trans/vip/translate",
-            query = baidu_translate_query(x, from = from, to = to)
+            query = baidu_translate_query(query, from = from, to = to)
         )
     url <- url(url, encoding = "utf-8")
     res <- jsonlite::fromJSON(url)
     
-    ret <- res$trans_result$dst
-    if (is.null(ret)) ret <- ""
-    return(ret)
+    return(res$trans_result$dst)
 }
 
 ##' @importFrom openssl md5
 baidu_translate_query <- function(x, from, to) {
     salt <- sample.int(1e+05, 1) 
-    .info <- get_translate_appkey('baidu')
+    .info <- get_translate_appkey()
     sign <- sprintf("%s%s%s%s", .info$appid, x, salt, .info$key)
     .sign <- openssl::md5(sign)
 
@@ -33,5 +28,4 @@ baidu_translate_query <- function(x, from, to) {
                 )
     return(.query)
 }
-
 
